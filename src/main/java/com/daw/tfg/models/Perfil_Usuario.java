89f2274d76aed62b2a93afadd4dbaa17626fe7ee
SergioGarLue <sergiogarcialuengo.03@gmail.com>
2026-01-26 16:49:34 +0100

    modelos Usuario y perfil_Usuario

--- src/main/java/com/daw/tfg/models/Perfil_Usuario.java
+++ src/main/java/com/daw/tfg/models/Perfil_Usuario.java
@@ -1,5 +1,44 @@
 package com.daw.tfg.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "Perfil_Usuario")
 public class Perfil_Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_perfil;
+
+    @Column(unique = true, nullable = false)
+    private String imagen_usuario;
+
+    @Column(unique = true, nullable = false)
+    private String imagen_fondo_perfil;
+
+    @Column(nullable = false)
+    private String pais;
+
+    @Column(nullable = false)
+    private String biografia;
+
+    @Column(nullable = false)
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "Usuario")
+    @JsonIgnore
+    private Usuario id_Usuario;
 
 }
--- src/main/java/com/daw/tfg/models/Usuario.java
+++ src/main/java/com/daw/tfg/models/Usuario.java
@@ -1,5 +1,36 @@
 package com.daw.tfg.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre_usuario;
+
+    @Column(nullable = false)
+    private String contraseña_cifrada;
+
+    @Column(unique = true, nullable = false)
+    private String correo_electronico;
+
+    @Column(unique = true, nullable = false)
+    private Boolean estado;
+
+    
 }
