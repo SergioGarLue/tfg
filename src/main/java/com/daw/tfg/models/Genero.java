c58a759834fbf460d04dbfa03c8b459d026972b6
zx22student <sergiogarcialuengo.03@gmail.com>
2026-01-27 11:18:25 +0100

    Cambio nombre tablas, comentarios

--- src/main/java/com/daw/tfg/models/Genero.java
+++ src/main/java/com/daw/tfg/models/Genero.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "generos")
+@Table(name = "genero")
 @Getter @Setter @NoArgsConstructor @ToString
 public class Genero {
     @Id
--- src/main/java/com/daw/tfg/models/Lista_Deseados.java
+++ src/main/java/com/daw/tfg/models/Lista_Deseados.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 
 @Entity
 @Getter @Setter @NoArgsConstructor @ToString
-@Table(name = "usuarios")
+@Table(name = "usuario_lista_deseados")
 public class Lista_Deseados {
 
 
