9f1fe5bde86248ca895fede24cd65d3874d4c593
SergioGarLue <sergiogarcialuengo.03@gmail.com>
2026-02-03 18:03:06 +0100

    usuarioService y necesidades del mismo

--- src/main/java/com/daw/tfg/Configuration/SecurityConfig.java
+++ src/main/java/com/daw/tfg/Configuration/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.daw.tfg.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,5 +16,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        //creamos los filtros
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/").permitAll() //publico
+            .requestMatchers("/añadir/**").authenticated() //privado
+            .requestMatchers("/registro").permitAll() //publico
+            .anyRequest().permitAll() //cualquier ruta no registrada
+        )
+        .formLogin(Customizer.withDefaults())
+        .logout(Customizer.withDefaults());
+        
+        return http.build(); // devolvemos los filtros que hemos creado
+    }
 }
--- src/main/java/com/daw/tfg/Dtos/UsuarioDTO.java
+++ src/main/java/com/daw/tfg/Dtos/UsuarioDTO.java
@@ -1,5 +1,20 @@
 package com.daw.tfg.Dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter @NoArgsConstructor
 public class UsuarioDTO {
+    @NotBlank
+    @Size(min=4, max=8)
+    private String username;
+
+    @NotBlank
+    @Size(min=8)
+    private String passwd;
 
 }
