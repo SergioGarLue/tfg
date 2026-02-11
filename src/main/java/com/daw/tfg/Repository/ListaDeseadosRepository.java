7a104a6958d2c2142ac9cb0e2e575cf8be810034
N03XCUS3S <zx22student3031@colegiovalledelmiro.es>
2026-02-11 08:41:55 +0100

    Update de los Repositories faltantes

--- src/main/java/com/daw/tfg/Repository/ListaDeseadosRepository.java
+++ src/main/java/com/daw/tfg/Repository/ListaDeseadosRepository.java
@@ -6,11 +6,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.daw.tfg.models.Juego;
-import com.daw.tfg.models.Lista_Deseados;
+import com.daw.tfg.models.ListaDeseados;
 import com.daw.tfg.models.Usuario;
 
-public interface ListaDeseadosRepository extends JpaRepository<Lista_Deseados, Long> {
-    Optional<Lista_Deseados> findByUsuario(Usuario usuario);
-    Optional<Lista_Deseados> findByUsuarioIdUsuario(Long idUsuario);
-    List<Lista_Deseados> findByJuegosContains(Juego juego);
+public interface ListaDeseadosRepository extends JpaRepository<ListaDeseados, Long> {
+    Optional<ListaDeseados> findByUsuario(Usuario usuario);
+    Optional<ListaDeseados> findByUsuarioIdUsuario(Long idUsuario);
+    List<ListaDeseados> findByJuegosContains(Juego juego);
 }
